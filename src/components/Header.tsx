b66c08ec02198dc47145087bdd09a4c3eaba9414
Mike-casper <misha7709123@gmail.com>
2023-01-04 18:52:18 +0300

    Step 15

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+export const Header = () => {
+    return(
+    <header className="header">
+            <img src={"https://cdn.iconscout.com/icon/free/png-256/wifi-443-470342.png"}/>
+    </header>
+    )
+}
