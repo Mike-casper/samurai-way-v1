b396569f02c827cbf7b431ef9a1a586b4d996707
Mike-casper <misha7709123@gmail.com>
2023-01-12 15:16:30 +0300

    Step 19 add route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
+import {Route} from "react-router-dom";
 
 
 function App() {
@@ -12,8 +13,9 @@ function App() {
             <Header/>
             <Navbar/>
             <div className="app-wrapper-content">
-                <Dialogs/>
-                {/*<Profile/>*/}
+                <Route component={Dialogs}/>
+                <Route component={Profile}/>
+
             </div>
         </div>);
 }
